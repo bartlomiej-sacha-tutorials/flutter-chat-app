cb820da17b77dbefd1d3ec3e6212e5e958bdd0a4
bartlomiej-sacha <sacha.bartlomiej@gmail.com>
2025-04-03 19:22:35 +0200

    finish without notifications

---
+++ lib/screens/splash.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(),
+            const SizedBox(height: 20),
+            Text(
+              'Loading...',
+              style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
